d7066f1f781ef6ce5850e78a2cf0931ba2648301
Lantz Warrick <betalantz@gmail.com>
2020-05-06 22:51:12 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -11,6 +11,12 @@ end
 def not_safe?(speed)
 	speed > 60 || speed < 40 ? true : false
 end
+
+def less_than_safe?(speed)
+  speed > 60 || speed < 40 ? "What a good driver!" : "Watch out!"
+end
+
+puts less_than_safe(55)
 	
 
 # Ternary syntax (mirrors if...else...end):
